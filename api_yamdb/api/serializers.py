aae602783d9e8384d954979958c1233f4e1d6526
Anton Lebedev <46112403+wrawka@users.noreply.github.com>
2021-09-08 22:48:55 +0300

    added serilizers

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from reviews.models import Comment, Review
+from rest_framework import serializers, validators
+
+User = get_user_model()
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
+    title = serializers.SlugRelatedField(
+        read_only=True, slug_field='pk')
+    review = serializers.SlugRelatedField(
+        read_only=True, slug_field='pk')
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
+    title = serializers.SlugRelatedField(
+        read_only=True, slug_field='pk')
+
+    class Meta:
+        model = Review
+        fields = '__all__'
