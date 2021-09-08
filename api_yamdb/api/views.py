f18a37a4defa8d71970e9ed70a5738924d6af2fb
Anton Lebedev <46112403+wrawka@users.noreply.github.com>
2021-09-08 22:48:41 +0300

    added views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,19 +1,43 @@
+from reviews.models import Review, Title
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from rest_framework import filters, mixins, pagination, permissions, viewsets
-
-Title = ...
+from . import serializers
 
 
 User = get_user_model()
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    pass
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [ ... ]
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        title = get_object_or_404(Title, pk=title_id)
+        review = get_object_or_404(Review, pk=review_id, title=title)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        title = get_object_or_404(Title, pk=title_id)
+        review = get_object_or_404(Review, pk=review_id, title=title)
+        serializer.save(title=title, review=review, author=self.request.user)
+
 
 class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ReviewSerializer
+    permission_classes = [ ... ]
+
     def get_queryset(self):
         title_id = self.kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
         return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        serializer.save(title=title, author=self.request.user)
