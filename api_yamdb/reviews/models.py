9c5608ffe788758b3e9283a67003f0d263cb27e1
Anton Lebedev <46112403+wrawka@users.noreply.github.com>
2021-09-08 19:00:02 +0300

    reviews app started

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+class Comment(models.Model):
+    text = models.TextField('Текст комментария')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        'Review', on_delete=models.CASCADE, related_name='comments')
+    pub_date = models.DateTimeField(
+        'Дата публикации комментария', auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.text[20:]}... - {self.author}@{self.pub_date}'
+
+
+class Review(models.Model):
+    text = models.TextField('Текст отзыва')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    score = models.IntegerField('Оценка')
+    pub_date = models.DateTimeField(
+        'Дата публикации отзыва', auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.text[20:]}... - {self.author}@{self.pub_date}'
