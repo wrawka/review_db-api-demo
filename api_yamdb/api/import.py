918a32607387e3284ea1af18e43e2265446d1eb5
Anton Lebedev <46112403+wrawka@users.noreply.github.com>
2021-09-16 00:16:20 +0300

    import.py flake8

--- api_yamdb/api/import.py
+++ api_yamdb/api/import.py
@@ -4,10 +4,12 @@ import sqlite3
 con = sqlite3.connect('/Users/maximagarev/Dev/api_yamdb/api_yamdb/db.sqlite3')
 cur = con.cursor()
 
-with open('/Users/maximagarev/Dev/api_yamdb/api_yamdb/static/data/category.csv','r') as f:
+path = '/Users/maximagarev/Dev/api_yamdb/api_yamdb/static/data/category.csv'
+with open(path, 'r') as f:
     dr = csv.DictReader(f, delimiter=",")
-    to_db = [(i['id'], i['name'],  i['slug']) for i in dr]
+    to_db = [(i['id'], i['name'], i['slug']) for i in dr]
 
-cur.executemany("INSERT INTO main.reviews_category(id, name, slug) VALUES (?, ?, ?);", to_db)
+cur.executemany("INSERT INTO main.reviews_category(id, name, slug)"
+                " VALUES (?, ?, ?);", to_db)
 con.commit()
 con.close()
