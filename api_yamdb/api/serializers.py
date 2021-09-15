5bb1f67b0ec7756d67cc6b463e361ae98efc52f9
Anton Lebedev <46112403+wrawka@users.noreply.github.com>
2021-09-16 00:10:04 +0300

    cleaned api/serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 import datetime as dt
 
-from rest_framework import serializers, validators, exceptions
+from rest_framework import exceptions, serializers
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import CHOICES, Code, User
 
@@ -60,7 +60,8 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username', default=serializers.CurrentUserDefault())
+        read_only=True, slug_field='username',
+        default=serializers.CurrentUserDefault())
     title = serializers.SlugRelatedField(
         read_only=True, slug_field='id', default=0)
 
@@ -75,9 +76,11 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         title = self.context.get('view').kwargs.get('title_id')
-        review = Review.objects.filter(title=title, author=self.context['request'].user)
+        review = Review.objects.filter(
+            title=title, author=self.context['request'].user)
         if self.context['request'].method == 'POST' and review.exists():
-            raise exceptions.ValidationError('You already have a review on this title')
+            raise exceptions.ValidationError('You already have a review'
+                                             'on this title')
         return super().validate(attrs)
 
 
@@ -100,12 +103,15 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class TitleSerializer(serializers.ModelSerializer):
     rating = serializers.SerializerMethodField()
-    genre = serializers.SlugRelatedField(queryset=Genre.objects.all(), slug_field='slug', many=True)
-    category = serializers.SlugRelatedField(queryset=Category.objects.all(), slug_field='slug')
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(), slug_field='slug', many=True)
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(), slug_field='slug')
 
     class Meta:
         model = Title
-        fields = ('id', 'name', 'year', 'rating', 'description', 'genre', 'category')
+        fields = ('id', 'name', 'year', 'rating', 'description', 'genre',
+                  'category')
 
     def validate_year(self, value):
         year = dt.datetime.today().year
@@ -116,12 +122,13 @@ class TitleSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         title = super().to_representation(instance)
         title['genre'] = GenreSerializer(instance.genre, many=True).data
-        title['category'] = CategorySerializer(instance.category, source=title).data
+        title['category'] = CategorySerializer(
+            instance.category, source=title).data
         return title
 
     def get_rating(self, obj):
         reviews = [review.score for review in obj.reviews.all()]
         if reviews:
-            return round(sum(reviews)/len(reviews))
+            return round(sum(reviews) / len(reviews))
         else:
             return None
