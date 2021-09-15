241120821e2507b74a992348c90f880deb8ecb95
Anton Lebedev <46112403+wrawka@users.noreply.github.com>
2021-09-16 00:10:32 +0300

    cleaned api/urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,4 @@
-from django.urls import include, path, re_path
-from rest_framework.authtoken import views
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (APITokenView, CategoryViewSet, CommentViewSet,
